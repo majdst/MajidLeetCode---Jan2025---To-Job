024013c9b0b15d49f43548d0421aa0f9ff788f08
Majid Dousti <dousti.explore@gmail.com>
2025-08-18 12:32:07 -0400

    shorter and easier one

--- LeetCode(TwoSum)/TwoSum.py
+++ LeetCode(TwoSum)/TwoSum.py
@@ -13,13 +13,24 @@ def gather_user():
     return list_desig
 
 def main():
+    # list1 = gather_user()
+    # print('What will be the target? ')
+    # target = int(input('Target: '))
+    # #BrutForce 
+    # for i in range(len(list1) - 1):
+    #     for j in range(i+1, len(list1)):
+    #         if list1[i] + list1[j] == target:
+    #             print(f'index are{[i, j]}')
     list1 = gather_user()
     print('What will be the target? ')
     target = int(input('Target: '))
-    #BrutForce 
-    for i in range(len(list1) - 1):
-        for j in range(i+1, len(list1)):
-            if list1[i] + list1[j] == target:
-                print(f'index are{[i, j]}')
+    dic_h = {}
+
+    for idx, n in enumerate(list1):
+        if (target - n) in dic_h:
+            print([dic_h[target-n], idx]) 
+        else:
+            dic_h[n] =idx
+        
             
 main()
